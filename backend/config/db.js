5ae42147e399e4b8c73845d73f6ce6682aae464c
Daniel Curtis <daniel17curtis@gmail.com>
2020-07-25 20:54:30 -0400

    Updated mongo URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,12 +4,15 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-	const conn = await mongoose.connect(process.env.MONGO_URI, {
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useFindAndModify: false,
-		useUnifiedTopology: true,
-	});
+	const conn = await mongoose.connect(
+		`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongo-cluster-ip-service:27017/${process.env.MONGO_INITDB_DATABASE}`,
+		{
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true,
+		}
+	);
 
 	console.log(`MongoDB Connected ${conn.connection.host}`);
 };
