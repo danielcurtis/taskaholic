d5d1fc7cc670011cbb2caa7ecec8d03c1a3b6c63
Daniel Curtis <daniel17curtis@gmail.com>
2020-07-26 13:24:11 -0400

    Added docker auth

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker login -u $DOCKER_ID -p $DOCKER_PASSWORD docker.io
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
 
 docker build -t curtiscodes/taskaholic-frontend:latest -t curtiscodes/taskaholic-frontend:$SHA -f ./frontend/Dockerfile ./frontend
 docker build -t curtiscodes/taskaholic-backend:latest -t curtiscodes/taskaholic-backend:$SHA -f ./backend/Dockerfile ./backend
 
-docker push curtiscodes/frontend:latest
-docker push curtiscodes/backend:latest
-docker push curtiscodes/frontend:$SHA
-docker push curtiscodes/backend:$SHA
+docker push curtiscodes/taskaholic-frontend:latest
+docker push curtiscodes/taskaholic-backend:latest
+docker push curtiscodes/taskaholic-frontend:$SHA
+docker push curtiscodes/taskaholic-backend:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/backend-deployment backend=curtiscodes/taskaholic-backend:$SHA
-kubectl set image deployments/frontend-deployment frontend=curtiscodes/taskaholic-frontend:$SHA
+kubectl set image deployments/taskaholic-backend-deployment backend=curtiscodes/taskaholic-backend:$SHA
+kubectl set image deployments/taskaholic-frontend-deployment frontend=curtiscodes/taskaholic-frontend:$SHA
