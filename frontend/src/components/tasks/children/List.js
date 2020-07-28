7210f2bc55f972db92bbe5e113c30b64ab76fcb4
Daniel Curtis <daniel17curtis@gmail.com>
2020-07-28 14:10:40 -0400

    Updated tasks UI

--- frontend/src/components/tasks/children/List.js
+++ frontend/src/components/tasks/children/List.js
@@ -1,23 +1,16 @@
 // @ts-check
 
 import React from 'react';
-import { FaTag, FaCalendarCheck } from 'react-icons/fa';
-import { FiClock } from 'react-icons/fi';
+import MapTasks from './MapTasks';
+import {
+	toDo,
+	inProgress,
+	paused,
+	completed,
+} from '../../utils/filterTasks.js';
+import Meter from '../../utils/Meter';
 
 function List({ setToggle, tasks, setCurrent }) {
-	const convertDate = (d) => {
-		let date = new Date(d);
-		return (
-			(date.getMonth() > 8
-				? date.getMonth() + 1
-				: '0' + (date.getMonth() + 1)) +
-			'/' +
-			(date.getDate() > 9 ? date.getDate() : '0' + date.getDate()) +
-			'/' +
-			date.getFullYear()
-		);
-	};
-
 	const handleTaskClick = (id) => {
 		setCurrent(id);
 		setToggle('Edit');
@@ -29,7 +22,7 @@ function List({ setToggle, tasks, setCurrent }) {
 		return (
 			<div>
 				<div className="flex">
-					<h1>Your Tasks</h1>
+					<h1>Your tasks</h1>
 					<button
 						onClick={() => setToggle('Create')}
 						style={{ marginLeft: '30px' }}>
@@ -37,38 +30,29 @@ function List({ setToggle, tasks, setCurrent }) {
 					</button>
 				</div>
 
-				<div className="Tags-list">
-					{tasks.map((task) => {
-						let time = 0;
-						task.timelog.map((el) => (time += parseInt(el[1])));
+				<Meter arr={tasks} />
 
-						return (
-							<div
-								key={task._id}
-								className="Tags-list-item"
-								onClick={() => handleTaskClick(task._id)}>
-								<h2>
-									<FaCalendarCheck
-										style={{ marginRight: '8px', color: '#007aff' }}
-									/>{' '}
-									{task.name}
-								</h2>
-								<p style={{ marginBottom: '8px', fontWeight: 'bold' }}>
-									{`${time} hours logged`}
-								</p>
-								<p style={{ marginTop: 0 }}>{task.description}</p>
-								<span className="Tags-list-item-meta">{task.status}</span>
-								<span className="Tags-list-item-meta">
-									<FaTag style={{ marginRight: '3px', fontSize: '10px' }} />
-									{task.tag.name}
-								</span>
-								<span className="Tags-list-item-meta">
-									<FiClock style={{ marginRight: '3px', fontSize: '10px' }} />
-									{convertDate(task.due)}
-								</span>
-							</div>
-						);
-					})}
+				<div className="Dashboard-tasks">
+					<MapTasks
+						tasks={toDo(tasks)}
+						name="To Do"
+						handleTaskClick={handleTaskClick}
+					/>
+					<MapTasks
+						tasks={inProgress(tasks)}
+						name="In Progress"
+						handleTaskClick={handleTaskClick}
+					/>
+					<MapTasks
+						tasks={completed(tasks)}
+						name="Completed"
+						handleTaskClick={handleTaskClick}
+					/>
+					<MapTasks
+						tasks={paused(tasks)}
+						name="Paused"
+						handleTaskClick={handleTaskClick}
+					/>
 				</div>
 			</div>
 		);
