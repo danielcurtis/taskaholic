24576878f2ccabcbb9b85827fd1e553e86ca09e6
Daniel Curtis <daniel17curtis@gmail.com>
2020-07-29 11:12:54 -0400

    Updated task list UI

--- frontend/src/components/tasks/children/MapTasks.js
+++ frontend/src/components/tasks/children/MapTasks.js
@@ -1,7 +1,7 @@
 // @ts-check
 
 import React from 'react';
-import { FaTag, FaCalendarCheck } from 'react-icons/fa';
+import { FaTag, FaCalendarCheck, FaBorderStyle } from 'react-icons/fa';
 import { FiClock } from 'react-icons/fi';
 import convertDate from '../../utils/convertDate';
 import colorFromName from '../../utils/colorFromName';
@@ -23,16 +23,18 @@ function MapTasks({ tasks, name, handleTaskClick }) {
 							key={task._id}
 							className="Tags-list-item"
 							onClick={() => handleTaskClick(task._id)}>
-							<h2>
-								<FaCalendarCheck
-									style={{ marginRight: '8px', color: '#007aff' }}
-								/>{' '}
+							<p
+								style={{
+									margin: '0 0 5px',
+									fontWeight: 'bold',
+								}}>
 								{task.name}
-							</h2>
-							<p style={{ marginBottom: '8px', fontWeight: 'bold' }}>
-								{`${time} hours logged`}
 							</p>
-							<p style={{ marginTop: 0 }}>{task.description}</p>
+							<p style={{ margin: '0 0 8px' }}>{task.description}</p>
+							<span className="Tags-list-item-meta">
+								<FiClock style={{ marginRight: '3px', fontSize: '10px' }} />
+								{time} hrs
+							</span>
 							<span className="Tags-list-item-meta">
 								<FaTag style={{ marginRight: '3px', fontSize: '10px' }} />
 								{task.tag.name}
