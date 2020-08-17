d1d6cd0661103a6d045c5f11e17664f7b6983c04
Daniel Curtis <daniel17curtis@gmail.com>
2020-08-17 18:59:30 -0400

    Added brackets around if statement

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,7 +7,6 @@ import Habits from '../components/habits/Habits';
 import Menu from '../components/menu/Menu';
 import Settings from '../components/settings/Settings';
 import Tasks from '../components/tasks/Tasks';
-import Timesheet from '../components/timesheet/Timesheet';
 
 function Home() {
 	// Init view & component
@@ -15,12 +14,17 @@ function Home() {
 	let component = <Dashboard />;
 
 	// Set view, similar to React Router
-	if (view === 'Dashboard') component = <Dashboard />;
-	else if (view === 'Tags') component = <Tags />;
-	else if (view === 'Habits') component = <Habits />;
-	else if (view === 'Settings') component = <Settings />;
-	else if (view === 'Tasks') component = <Tasks />;
-	else if (view === 'Timesheet') component = <Timesheet />;
+	if (view === 'Dashboard') {
+		component = <Dashboard />;
+	} else if (view === 'Tags') {
+		component = <Tags />;
+	} else if (view === 'Habits') {
+		component = <Habits />;
+	} else if (view === 'Settings') {
+		component = <Settings />;
+	} else if (view === 'Tasks') {
+		component = <Tasks />;
+	}
 
 	return (
 		<div>
