4429380ab79bb7f429dfa3f41b8279eb3e69c52d
Daniel Curtis <daniel17curtis@gmail.com>
2020-08-17 19:01:00 -0400

    Updated tags desktop UI

--- frontend/src/components/tags/Tags.js
+++ frontend/src/components/tags/Tags.js
@@ -2,14 +2,16 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Tag from './children/Tag';
 import Create from './children/Create';
-import List from './children/List';
 import Edit from './children/Edit';
 
 function Tags() {
-	const [toggle, setToggle] = useState('List');
 	const [tags, setTags] = useState([]);
+	const [edit, setEdit] = useState(false);
+	const [create, setCreate] = useState(false);
 	const [current, setCurrent] = useState('');
+	const [update, setUpdate] = useState(0);
 
 	useEffect(() => {
 		const getTags = async () => {
@@ -18,27 +20,59 @@ function Tags() {
 		};
 
 		getTags();
-	}, [toggle]);
+	}, [update]);
 
-	if (toggle === 'List') {
-		return (
-			<div>
-				<List setToggle={setToggle} tags={tags} setCurrent={setCurrent} />
+	return (
+		<div className="Tags">
+			<div className="flex">
+				<h1>Tags</h1>
+				<button onClick={() => setCreate(true)}>Create Tag</button>
 			</div>
-		);
-	} else if (toggle === 'Edit') {
-		return (
+
 			<div>
-				<Edit setToggle={setToggle} tags={tags} current={current} />
+				{tags.map((tag) => (
+					<Tag
+						key={tag.id}
+						tag={tag}
+						setEdit={setEdit}
+						setCurrent={setCurrent}
+					/>
+				))}
 			</div>
-		);
-	} else if (toggle === 'Create') {
-		return (
-			<div>
-				<Create setToggle={setToggle} />
-			</div>
-		);
-	}
+			{create ? (
+				<Create setCreate={setCreate} update={update} setUpdate={setUpdate} />
+			) : null}
+			{edit ? (
+				<Edit
+					tags={tags}
+					setEdit={setEdit}
+					current={current}
+					update={update}
+					setUpdate={setUpdate}
+				/>
+			) : null}
+		</div>
+	);
+
+	// if (toggle === 'List') {
+	// 	return (
+	// 		<div>
+	// 			<List setToggle={setToggle} tags={tags} setCurrent={setCurrent} />
+	// 		</div>
+	// 	);
+	// } else if (toggle === 'Edit') {
+	// 	return (
+	// 		<div>
+	// 			<Edit setToggle={setToggle} tags={tags} current={current} />
+	// 		</div>
+	// 	);
+	// } else if (toggle === 'Create') {
+	// 	return (
+	// 		<div>
+	// 			<Create setToggle={setToggle} />
+	// 		</div>
+	// 	);
+	// }
 }
 
 export default Tags;
