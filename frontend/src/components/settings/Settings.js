c39fff621eb55f5c8e5c32b15de6d164cd1ead37
Daniel Curtis <daniel17curtis@gmail.com>
2020-08-19 17:38:48 -0400

    Updating settings page with new email

--- frontend/src/components/settings/Settings.js
+++ frontend/src/components/settings/Settings.js
@@ -30,13 +30,13 @@ function Settings() {
 				Settings
 			</h1>
 			<p>
-				As a Beta user, you drive the Taskaholic product. Contact us for support
-				or suggestions.
+				Thanks for being a Beta user! If you have any questions or suggestions,
+				don't hesitate to email us!
 			</p>
 			<button className="blue-btn" onClick={logout}>
 				Log out
 			</button>
-			<a href="mailto:support@taskaholic.com">
+			<a href="mailto:contacttaskaholic@gmail.com">
 				<button>Email Us</button>
 			</a>
 			{loading ? null : (
@@ -45,6 +45,12 @@ function Settings() {
 					<Password user={user} />
 				</div>
 			)}
+			<h2>Feature Roadmap:</h2>
+			<ul style={{ marginBottom: 30 }}>
+				<li>Color Coded Tags</li>
+				<li>Calendar Time View</li>
+				<li>GitHub Integration</li>
+			</ul>
 			<p>
 				<a
 					href="https://docs.google.com/document/d/1h6rzrxmi0X2RLjMRX1IEB1U0orxDM7u8tvg86InHD1k/edit?usp=sharing"
