080268dbdfdd5d5bca27a2592bb3dc982a7ec7e4
Daniel Curtis <daniel17curtis@gmail.com>
2020-08-18 21:12:45 -0400

    Moved GET csrf cookie method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,12 @@
 // @ts-check
 
 import React, { useContext, useEffect } from 'react';
-import axios from 'axios';
 import { UserContext } from './context/User';
 import Home from './pages/Home';
 import Landing from './pages/Landing';
 
 function App() {
 	const { user } = useContext(UserContext);
-
-	useEffect(() => {
-		const getCsrfToken = async () => {
-			const { data } = await axios.get('/api/v1/auth/csrf-token');
-			axios.defaults.headers.post['X-CSRF-Token'] = data.csrfToken;
-			axios.defaults.headers.put['X-CSRF-Token'] = data.csrfToken;
-			axios.defaults.headers.delete['X-CSRF-Token'] = data.csrfToken;
-		};
-		getCsrfToken();
-	}, []);
 
 	return user.auth ? <Home /> : <Landing />;
 }
