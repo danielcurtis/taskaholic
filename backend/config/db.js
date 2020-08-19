f7620ee4e9bc926bafb21d23f91b0859cd686d44
Daniel Curtis <daniel17curtis@gmail.com>
2020-08-19 17:40:00 -0400

    Fixed connection bug

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,21 +4,17 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-	try {
-		const conn = await mongoose.connect(
-			`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@cluster0-shard-00-00.2rhro.mongodb.net:27017,cluster0-shard-00-01.2rhro.mongodb.net:27017,cluster0-shard-00-02.2rhro.mongodb.net:27017/${process.env.MONGO_INITDB_DATABASE}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
-			{
-				useNewUrlParser: true,
-				useCreateIndex: true,
-				useFindAndModify: false,
-				useUnifiedTopology: true,
-			}
-		);
+	const conn = await mongoose.connect(
+		`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@cluster0-shard-00-00.2rhro.mongodb.net:27017,cluster0-shard-00-01.2rhro.mongodb.net:27017,cluster0-shard-00-02.2rhro.mongodb.net:27017/${process.env.MONGO_INITDB_DATABASE}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
+		{
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true,
+		}
+	);
 
-		console.log(`MongoDB connected ${conn.connection.host}`);
-	} catch (err) {
-		console.log(`MongoDB could not connect: ${err}`);
-	}
+	console.log(`MongoDB connected ${conn.connection.host}`);
 };
 
 module.exports = connectDB;
